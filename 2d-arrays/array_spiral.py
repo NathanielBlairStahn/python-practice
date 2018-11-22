c305e23e2999472e3623379751659d0880cb5bd2
Nathaniel Blair-Stahn <nathaniel.blair.stahn@gmail.com>
2018-11-21 22:21:20 -0800

    oops, my new solution also has a bug...

--- 2d-arrays/array_spiral.py
+++ 2d-arrays/array_spiral.py
@@ -81,6 +81,42 @@ def array_spiral_orig(arr):
         left += 1
         right -= 1
 
+def array_spiral_orig_fixed(arr):
+    """
+    Fixed(?) version of the original I came up with.
+
+    Given a rectangular 2D array, yields the elements in a clockwise spiral order,
+    starting at the upper left corner and ending at the center of the array.
+
+    For example:
+
+    1  2  3  4
+    5  6  7  8   -->  1 2 3 4 8 12 11 10 9 5 6 7
+    9  10 11 12
+    """
+
+    #Orient the array in like a matrix, so the 1st index is the row
+    #and the 2nd index is the column.
+    #That is, arr[i][j] is the element in the ith row and jth column.
+    top = 0
+    bottom =len(arr)
+    left = 0
+    right = len(arr[0])
+
+    while top < bottom and left < right:
+        for j in range(left, right):
+            yield arr[top][j]
+        for i in range(top+1, bottom):
+            yield arr[i][right-1]
+        for j in range(right-2, left-1, -1):
+            yield arr[bottom-1][j]
+        for i in range(bottom-2, top, -1):
+            yield arr[i][left]
+
+        top += 1
+        bottom -= 1
+        left += 1
+        right -= 1
 
 if __name__=="__main__":
     arr = [[1,2,3,4], [5,6,7,8], [9,10,11,12]]
