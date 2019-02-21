373c1743cfe797cf38242c2cd0fd352b9e2ec7ce
Nathaniel Blair-Stahn <nathaniel.blair.stahn@gmail.com>
2019-02-21 12:44:19 -0800

    add Draw class to star diamond card simulation

--- problems/star_diamond_cards.py
+++ problems/star_diamond_cards.py
@@ -1,6 +1,57 @@
 import numpy as np
 
-def simulate_draw(num_cards=60, num_stars=3, num_diamonds=3, num_drawn=5, num_redrawn=3):
+class Draw:
+    """
+    Class to encode one complete "draw" of cards including discarding
+    stars and re-drawing.
+    """
+    def __init__(self, deck_size, initial_hand_size, next_card, stars, diamonds):
+        self.deck_size = deck_size
+        self.initial_hand_size = initial_hand_size
+        self.next_card = next_card
+        self.stars = stars
+        self.diamonds = diamonds
+        # self.num_to_redraw = num_to_redraw
+        # self.num_to_redraw = 1
+
+    def discarded_stars(self):
+        """
+        Returns the stars that were discarded.
+        """
+        return [star for star in self.stars if star < self.next_card]
+
+    def num_redrawn(self):
+        """
+        Returns the total number of cards redrawn.
+        """
+        return self.next_card - self.initial_hand_size - len(self.discarded_stars())
+
+    def cards_in_hand(self):
+        """
+        Returns a generator to iterate through the cards in the current hand.
+        """
+        return (card for card in range(min(self.next_card, self.deck_size))
+            if card not in self.stars)
+
+    def num_cards_in_hand(self):
+        """
+        Returns the number of cards in the current hand.
+        """
+        return len([self.cards_in_hand()])
+
+    def has_diamond(self):
+        """
+        Returns True if the hand contains a diamond.
+        """
+        return any(diamonds < self.next_card)
+
+    def is_success(self):
+        """
+        Returns True if we were able to discard all the stars and redraw before reaching the end of the deck.
+        """
+        return self.next_card <= self.deck_size
+
+def simulate_draw(num_cards=60, num_stars=3, num_diamonds=3, num_drawn=5, num_to_redraw=3):
     '''
     You have a shuffled deck of 60 cards containing the following
     cards of special interest:
@@ -44,7 +95,7 @@ def simulate_draw(num_cards=60, num_stars=3, num_diamonds=3, num_drawn=5, num_re
     # next_star_idx=0
     # next_card = num_drawn #The next card in the deck after we have drawn the top num_drawn cards
     # while next_star_idx < len(stars) and stars[next_star_idx] < next_card:
-    #     next_card += num_redrawn
+    #     next_card += num_to_redraw
     #     next_star_idx += 1
 
     #3rd Attempt:
@@ -55,12 +106,13 @@ def simulate_draw(num_cards=60, num_stars=3, num_diamonds=3, num_drawn=5, num_re
         #If the next star is less than the next card to draw, then it is in our hand,
         #so we must discard it and draw 3 more.
         if star < next_card:
-            next_card += num_redrawn
+            next_card += num_to_redraw
         #Otherwise, there are no stars in our hand, so we're done drawing cards.
         else:
             break
 
-    return next_card, stars, diamonds
+    # return next_card, stars, diamonds
+    return Draw(num_cards, num_drawn, next_card, stars, diamonds)
 
 def simulate_diamond_prob():
     pass
