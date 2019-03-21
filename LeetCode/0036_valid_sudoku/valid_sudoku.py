53b85866cf1dcc111e8bbbdc86b518bbb5813559
Nathaniel Blair-Stahn <nathaniel.blair.stahn@gmail.com>
2019-03-21 13:17:17 -0700

    work on valid sudoku

--- LeetCode/0036_valid_sudoku/valid_sudoku.py
+++ LeetCode/0036_valid_sudoku/valid_sudoku.py
@@ -51,37 +51,97 @@ The given board contain only digits 1-9 and the character '.'.
 The given board size is always 9x9.
 """
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        value_counter = {str(i): 0 for i in range(1,10)}
+    def isValidSudoku(self, board):
+        # value_counter = {str(i): 0 for i in range(1,10)}
 
         # Check rows
-        for row in board:
-            for num in row:
-                if num != '.':
-                    value_counter[num] += 1
-            if any(value_counter[num] > 1 for num in value_counter):
-                return False
+        # for row in board:
+        #     for num in row:
+        #         if num != '.':
+        #             value_counter[num] += 1
+        #     if any(value_counter[num] > 1 for num in value_counter):
+        #         return False
+        #
+        # # Reset counter
+        # for num in value_counter:
+        #     value_counter[num] = 0
 
-        # Reset counter
-        for num in value_counter:
-            value_counter[num] = 0
+        # board is itself a list of rows
+        rows = board
+        if not self.isValidCollectionOfGroups(rows): return False
 
         # Check columns
         # for c in range(9):
         #     for r in range(9):
         #         if board[r][c] != '.':
         #             value_counter[board[r][c]] += 1
-        for col in zip(*board):
-            for num in col:
+        #
+        # for col in zip(*board):
+        #     for num in col:
+        #         if num != '.':
+        #             value_counter[num] += 1
+        #     if any(value_counter[num] > 1 for num in value_counter):
+        #         return False
+        #
+        # # Reset counter
+        # for num in value_counter:
+        #     value_counter[num] = 0
+
+        # Transpose board using zip(*board) to get list of columns
+        columns = zip(*board)
+        if not self.isValidCollectionOfGroups(columns): return False
+
+        # Check subsquares
+        #
+        subsquares = [[board[square//3 + i][square%3 + j] for i in range(3) for j in range(3)] for square in range(9)]
+        if not self.isValidCollectionOfGroups(subsquares): return False
+
+        return True
+
+    def isValidCollectionOfGroups(self, collection):
+        """A "group" can be a row, columm, or 3x3 subsquare.
+        A collection of groups can be all rows, all columns, or all 3x3 subsquares.
+        """
+        value_counter = {str(i): 0 for i in range(1,10)}
+        for group in collection:
+            for num in group:
                 if num != '.':
                     value_counter[num] += 1
             if any(value_counter[num] > 1 for num in value_counter):
                 return False
-
-        # Reset counter
-        for num in value_counter:
-            value_counter[num] = 0
-
-        # Check subsquares
+            #Reset counter after each group
+            for num in value_counter:
+                value_counter[num] = 0
 
         return True
+
+if __name__=="__main__":
+    solution = Solution()
+
+    board1 = [
+      ["5","3",".",".","7",".",".",".","."],
+      ["6",".",".","1","9","5",".",".","."],
+      [".","9","8",".",".",".",".","6","."],
+      ["8",".",".",".","6",".",".",".","3"],
+      ["4",".",".","8",".","3",".",".","1"],
+      ["7",".",".",".","2",".",".",".","6"],
+      [".","6",".",".",".",".","2","8","."],
+      [".",".",".","4","1","9",".",".","5"],
+      [".",".",".",".","8",".",".","7","9"]
+    ]
+
+    print(solution.isValidSudoku(board1))
+
+    board2 = [
+       ["8","3",".",".","7",".",".",".","."],
+       ["6",".",".","1","9","5",".",".","."],
+       [".","9","8",".",".",".",".","6","."],
+       ["8",".",".",".","6",".",".",".","3"],
+       ["4",".",".","8",".","3",".",".","1"],
+       ["7",".",".",".","2",".",".",".","6"],
+       [".","6",".",".",".",".","2","8","."],
+       [".",".",".","4","1","9",".",".","5"],
+       [".",".",".",".","8",".",".","7","9"]
+    ]
+
+    print(solution.isValidSudoku(board2))
