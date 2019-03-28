6980681d275b92c32e5b22632d52ccdaec7674a4
Nathaniel Blair-Stahn <nathaniel.blair.stahn@gmail.com>
2019-03-28 16:32:15 -0700

    add longest palindrome solution

---
+++ LeetCode/0005_longest_palindromic_substring/longest_palindrome.py
@@ -0,0 +1,56 @@
+"""
+5. Longest Palindromic Substring
+
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+Example 2:
+
+Input: "cbbd"
+Output: "bb"
+"""
+
+def longest_initial_palindrome(s: str) -> str:
+    """Returns the longest palindrome starting at the beginning of s"""
+    start = 0
+    end = len(s)-1
+
+    # Walk backwards through string:
+    # 1. If current last letter equals the beginning letter,
+    #    we might have a palindrome, so advance the start
+    #    pointer to keep checking.
+    # 2. If start and end don't match, the current end letter
+    #    can't be in the longest palindrome, so reset start to 0.
+    # 3. Decrement the end pointer in either case to continue
+    #    walking backwards through string.
+    # 4. We've found a palindrome when the pointers meet
+    #    in the middle.
+    while start < end:
+        if s[start] == s[end]:
+            start += 1
+        else:
+            start = 0
+        end -= 1
+
+    # Suppose the longest palindrome has length k.
+    # When the loop exits, either
+    # (1) start == end at the middle index when k is odd, or
+    # (2) start == end + 1 when k is even, with end being
+    # the last index of the first half of the string, and
+    # start being the first index of the second half.
+    return s[start+end+1]
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest = ""
+        for i in range(len(s)):
+            pal = longest_initial_palindrome(s[i:])
+            if len(pal) > len(longest):
+                longest = pal
+            if len(longest) >= len(s)-i-1: # No remaining substrings can be longer
+                break
+        return longest
