5e2940b6c4ed2ba6534ceb22d591bf0818520518
Nathaniel Blair-Stahn <nathaniel.blair.stahn@gmail.com>
2019-03-28 22:43:42 -0700

    try to fix longest palindrome solution

--- LeetCode/0005_longest_palindromic_substring/longest_palindrome.py
+++ LeetCode/0005_longest_palindromic_substring/longest_palindrome.py
@@ -19,6 +19,16 @@ def longest_initial_palindrome(s: str) -> str:
     start = 0
     end = len(s)-1
 
+    # The first two while loops stip of leading and trailing characters equal to s[0].
+    # Without this step, the method fails for, e.g.
+    # 'abaa', 'aaaabaaaaaaaaaa', 'aaabbcbbaaaaa',
+    # where the number of trailing a's is greater than the number of leading a's.
+    while start < end and s[start] == s[0] and s[end] == s[0]:
+        start += 1
+        end -= 1
+    while start < end and s[end] == s[0]:
+        end -= 1
+
     # Walk backwards through string:
     # 1. If current last letter equals the beginning letter,
     #    we might have a palindrome, so advance the start
@@ -42,7 +52,7 @@ def longest_initial_palindrome(s: str) -> str:
     # (2) start == end + 1 when k is even, with end being
     # the last index of the first half of the string, and
     # start being the first index of the second half.
-    return s[start+end+1]
+    return s[:start+end+1]
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
@@ -54,3 +64,11 @@ class Solution:
             if len(longest) >= len(s)-i-1: # No remaining substrings can be longer
                 break
         return longest
+
+
+def main():
+    # Note: My second solution passed 100/103 tests, and failed on the following:
+    s = "ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkbdhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgxoruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygjjptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeqdlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtjncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdquutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarabcroljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcudlzfzutplbycejmkpxcygsafzkgudy"
+
+    output = "riir"
+    expected = "fklkf"
